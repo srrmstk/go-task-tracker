3213c325140662f5a0f0eaaa141d028f99815643
srrmstk <sorrymistake@outlook.com>
2025-01-27 10:24:24 +0600

    feat: init migration

---
+++ migrations/20250127040314_init.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS tasks;
+-- +goose StatementEnd
