a6dc7f1faf31910e150e2b2e7b1190930fe9e4db
srrmstk <flygomail@gmail.com>
2025-07-26 00:47:41 +0600

    fix: add content-type header to jsonerror

--- internal/http-server/helpers/json_error.go
+++ internal/http-server/helpers/json_error.go
@@ -6,6 +6,7 @@ import (
 )
 
 func JsonError(w http.ResponseWriter, err string, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(map[string]string{
 		"error": err,
